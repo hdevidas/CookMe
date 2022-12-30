476dbdf5a5de494982cdacc28ac42a8770d388e1
hdevidas <hugodevidas@gmail.com>
2022-12-30 19:52:49 +0100

    merge branch tentatives

--- angular/src/app/components/header/header.component.ts
+++ angular/src/app/components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  logged = false;
+
+  ngOnInit(): void {
+    
+  }
+
+  logOn(): void {
+    this.logged = true;
+  }
+
+  logOff(): void {
+    this.logged = false;
+  }
 
 }
